048eb478da74579f30969d4f31df4ef7fa8968f6
Aimer-lllya <986104289@qq.com>
2020-08-09 16:25:52 +0800

    update

--- rw_visual.py
+++ rw_visual.py
@@ -4,7 +4,7 @@ from random_walk import RandomWalk
 # 只要程序处于活动状态，就不断地模拟随机漫步
 while True:
     # 创建一个RandomWalk类实例，并将其包含的点绘制出来
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_point))
@@ -13,6 +13,10 @@ while True:
     # 突出起点和终点
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # 隐藏坐标轴
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
